9ceb086dab92e5d71954d2a1f72a21e4d7b70c85
CamelGo <375836945@qq.com>
2015-01-17 16:11:26 +0800

    matching test

--- User_Src/spi.h
+++ User_Src/spi.h
@@ -1,30 +1,22 @@
-#ifndef __SPI_H
-#define __SPI_H
-#include "sys.h"
-//////////////////////////////////////////////////////////////////////////////////	 
-//本程序只供学习使用，未经作者许可，不得用于其它任何用途
-//Mini STM32开发板
-//SPI 驱动函数	   
-//正点原子@ALIENTEK
-//技术论坛:www.openedv.com
-//修改日期:2010/6/13 
-//版本：V1.0
-//版权所有，盗版必究。
-//Copyright(C) 正点原子 2009-2019
-//All rights reserved
-////////////////////////////////////////////////////////////////////////////////// 	  
+#ifndef _SPI_H_
+#define _SPI_H_
+#include "stm32f10x.h"
+/*
+6.2.4G:	        CE:PA15
+		CSN:PA4
+		SCK:PA5
+		MOSI:PA7
+		MISO:PA6
+		IRQ:PA8
+*/
+#define SPI_CE_H()   GPIO_SetBits(GPIOA, GPIO_Pin_15) 
+#define SPI_CE_L()   GPIO_ResetBits(GPIOA, GPIO_Pin_15)
 
-				    
-// SPI总线速度设置 
-#define SPI_SPEED_2   0
-#define SPI_SPEED_4   1
-#define SPI_SPEED_8   2
-#define SPI_SPEED_16  3
-#define SPI_SPEED_256 4
-						  	    													  
-void SPI1_Init(void);			 //初始化SPI1口
-void SPI1_SetSpeed(u8 SpeedSet); //设置SPI速度   
-u8   SPI1_ReadWriteByte(u8 TxData);//SPI总线读写一个字节
-		 
+#define SPI_CSN_H()  GPIO_SetBits(GPIOA, GPIO_Pin_4)
+#define SPI_CSN_L()  GPIO_ResetBits(GPIOA, GPIO_Pin_4)
+
+void SPI1_INIT(void);
+u8 SPI_RW(u8 dat);
+
 #endif
 
